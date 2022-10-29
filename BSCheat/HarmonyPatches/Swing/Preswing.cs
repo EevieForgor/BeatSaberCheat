55598045497a6f9186a2a08426e039df1070a16d
eeying <eeyingforgor@gmail.com>
2022-10-30 00:45:07 +0100

    Add project files.

---
+++ BSCheat/HarmonyPatches/Swing/Preswing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+
+/// <summary>
+/// See https://github.com/pardeike/Harmony/wiki for a full reference on Harmony.
+/// </summary>
+namespace BSCheat.HarmonyPatches.Swing
+{
+    /// <summary>
+    /// This patches ClassToPatch.MethodToPatch(Parameter1Type arg1, Parameter2Type arg2)
+    /// </summary>
+    [HarmonyPatch(typeof(SaberSwingRating), "BeforeCutStepRating")]
+    public class Preswing
+    {
+        static void Postfix(ref float __result)
+        {
+            // perfect preswing
+            __result = 100f;
+        }
+    }
+}
