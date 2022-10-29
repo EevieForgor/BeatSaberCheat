f2419856880b52425a80387959ebae9def89beaf
eeying <eeyingforgor@gmail.com>
2022-10-30 00:58:04 +0100

    Perfect Accuracy (15 points for center cut)

---
+++ BSCheat/HarmonyPatches/Accuracy/CenterDistance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+/// <summary>
+/// See https://github.com/pardeike/Harmony/wiki for a full reference on Harmony.
+/// </summary>
+namespace BSCheat.HarmonyPatches.Swing
+{
+    /// <summary>
+    /// This patches ClassToPatch.MethodToPatch(Parameter1Type arg1, Parameter2Type arg2)
+    /// </summary>
+    [HarmonyPatch(typeof(GameNoteController), "HandleCut")]
+    public class CenterDistance
+    {
+        static void Prefix(ref GameNoteController __instance, Saber saber, Vector3 cutPoint, Quaternion orientation, Vector3 cutDirVec, bool allowBadCut)
+        {
+            __instance.noteTransform.position = cutPoint;
+        }
+    }
+}
